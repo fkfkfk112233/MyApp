88541d894534dff7b93316328686b23c7ffd3e93
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-25 16:12:27 +0800

    Update WeatherController.java

--- backend/src/main/java/backend/controller/WeatherController.java
+++ backend/src/main/java/backend/controller/WeatherController.java
@@ -1,7 +1,5 @@
 package backend.controller;
 
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
