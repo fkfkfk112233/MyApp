5c7ed66170bc22a547fb5281d0ad3363287c5e61
fkfkfk112233 <kk60607kk@gmail.com>
2026-09-01 10:40:34 +0800

    Update weatherService.js

--- frontend/src/services/weatherService.js
+++ frontend/src/services/weatherService.js
@@ -1,7 +1,13 @@
-export async function getWeather(city) {
-  const response = await fetch(
-    `http://localhost:8080/api/weather?city=${encodeURIComponent(city)}`
-  );
+export async function getWeather(location) {
+  let url;
+
+  if (location.type === "city") {
+    url = `http://localhost:8080/api/weather?city=${encodeURIComponent(
+      location.city,
+    )}`;
+  }
+
+  const response = await fetch(url);
 
   if (!response.ok) {
     throw new Error("API request failed");
